d6c03c6ab6ed7afe8ac4533312be2de934233e63
Zhenguo <jingle1267@163.com>
2015-05-19 22:32:08 +0800

    [Add] Add Glide demo and android-support-v4.jar

--- SampleProject/src/com/ihongqiqu/common/library/sample/glide/GlideDemoActivity.java
+++ SampleProject/src/com/ihongqiqu/common/library/sample/glide/GlideDemoActivity.java
@@ -3,6 +3,9 @@ package com.ihongqiqu.common.library.sample.glide;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import com.ihongqiqu.common.library.sample.R;
 
 /**
  * Glide demo
@@ -19,6 +22,22 @@ public class GlideDemoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_glide);
+
+        ImageView img_first = (ImageView) findViewById(R.id.img_first);
+        ImageView img_second = (ImageView) findViewById(R.id.img_second);
+
+        Glide.with(GlideDemoActivity.this)
+                .load("https://raw.githubusercontent.com/jingle1267/TestResource/master/imgs/ic_launcher_128.png")
+                .centerCrop()
+                .into(img_first);
+
+        Glide.with(GlideDemoActivity.this)
+                .load("https://avatars2.githubusercontent.com/u/3887795?v=2&s=400")
+                .centerCrop()
+                .placeholder(R.drawable.ic_launcher)
+                .crossFade()
+                .into(img_second);
     }
 
 }
